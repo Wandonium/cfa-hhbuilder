36f6cde4f8a0009d342f4877f340634d649d1355
Wandonium <hillarywando@gmail.com>
2022-06-07 11:59:42 +0300

    Project complete

--- app.py
+++ app.py
@@ -6,18 +6,27 @@ slcsp = pd.read_csv('slcsp.csv')
 
 zipcode = slcsp.loc[26].zipcode
 for index, row in slcsp.iterrows():
-    zipcode = row.zipcode
-    myzips = zips[zips["zipcode"] == zipcode]
+    myzips = zips[zips["zipcode"] == row.zipcode]
     rate_areas = myzips['rate_area'].tolist()
-    # remove duplicates
+    # remove duplicates from rate areas
     rate_areas = list(dict.fromkeys(rate_areas))
-    for ra in rate_areas:
+    slcsp_rate = ''
+    if(len(rate_areas) > 1):
+        slcsp_rate = ''
+    else:
+        ra = rate_areas[0]
         zipcodePlans = plans[(plans["rate_area"] == ra) & (plans["metal_level"] == 'Silver')]
-        print("no. of zipcode plans: ", len(zipcodePlans.index))
-        silverRates = zipcodePlans['rate'].tolist()
-        print('silver rates: ', len(silverRates))
-# theRateArea = myzips.iloc[0].rate_area
-# print('theRateArea: ', theRateArea)
-# for index, row in myzips.iterrows():
-#     if(row.loc["rate_area"] != theRateArea):
-#         print(f'zipcode ${zipcode} has multiple rate areas in zips.csv: ${theRateArea} and ${row.loc["rate_area"]}')
+        noOfRows = len(zipcodePlans.index)
+        if(noOfRows <= 1):
+            slcsp_rate = ''
+        else:
+            silverRates = zipcodePlans['rate'].tolist()
+            # remove duplicates from silver rates
+            silverRates = list(dict.fromkeys(silverRates))
+            silverRates.sort()
+            slcsp_rate = format(silverRates[1], '.2f')
+    slcsp.at[index, 'rate'] = slcsp_rate
+print(slcsp.to_csv(index=False))
+
+# comment this in to print out the rates into the slcsp file
+# slcsp.to_csv('slcsp.csv', index=False)
